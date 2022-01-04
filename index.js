c3aa57c25a11b8da5396c7bc2af0eea96cff6714
marvinlartey <75137498+marvinlartey@users.noreply.github.com>
2022-01-04 15:10:47 +0000

    updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import contactsModel from './schema/schema.js';
+
 
 
 
@@ -15,3 +19,57 @@ app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
 const db = process.env.DB_URL
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log(`connected to DB`))
+.catch(err => console.log(err));
+
+
+app.get('/', async(req,res)=>{
+
+    res.json({
+        message:'Welcome to the contacts backend API'
+    })
+})
+
+
+app.get(`/contacts`,async(req,res)=>{
+    const allContacts = await contactsModel.find({});
+
+    if (allContacts){
+        //success
+        return res.status(200).json({
+            message: `Contacts fetched successfully`,
+            data: allContacts
+        });
+    }else {
+        //error
+        return res.status(500).json({
+            message: `Oops! unable to fetch contacts`
+        });
+    }
+})
+
+
+//Get all group contacts
+
+app.get('/contacts/:group', async (req,res)=>{
+    const {group}= req.params;
+    const allGroupContacts= await contactsModel.find({}).where("group").equals(group);
+
+    if(allGroupContacts){
+         return res.status(200).json({
+             message: 'Contacts group fetched successfully',
+             data: allGroupContacts
+         });
+    }else{
+        return res.status(500).json({
+            message: `Unable to fetch ${group} contacts`
+        });
+    }
+})
+
+app.listen((PORT),() => {
+    console.log(`Your app listening on port ${PORT}`);});
--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,28 @@
 import mongoose from 'mongoose';
 const {Schema,model} = mongoose;
 
+const contactsSchema = Schema({
+    contactName:{
+        type : String,
+        required : true
 
+    },
+
+    contactNumber:{
+        type:String,
+        required:true 
+    },
+
+    contactEmail:{
+        type: String,
+        required: false
+    },
+
+    contactGroup:{
+        type: String,
+        required: false
+    }
+});
+
+const contactsModel= model('contact', contactsSchema);
+export default contactsModel;
